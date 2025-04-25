9e1663e795127ee482c984f20ca7b814b04f14c2
Alvin Mugoya <mugoyaalvin018@gmail.com>
2025-04-25 14:50:18 +0300

    Update even_or_odd.py

--- even_or_odd.py
+++ even_or_odd.py
@@ -4,3 +4,8 @@ def check_even_odd(num):
         return "Even"
     else:  # Otherwise, it's odd
         return "Odd"
+        # Calling the function with different numbers
+print(check_even_odd(4))     # Output: Even (4 % 2 == 0)
+print(check_even_odd(7))     # Output: Odd (7 % 2 != 0)
+print(check_even_odd(0))     # Output: Even (0 % 2 == 0)
+
