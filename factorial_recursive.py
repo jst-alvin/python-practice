616ced0c402ba73b9ac2edb4d73222c4ecde1b09
Alvin Mugoya <mugoyaalvin018@gmail.com>
2025-04-25 14:45:39 +0300

    Update factorial_recursive.py

--- factorial_recursive.py
+++ factorial_recursive.py
@@ -5,3 +5,8 @@ def factorial_recursive(n):
     else:
         # Recursive case: n! = n * (n-1)!
         return n * factorial_recursive(n - 1)
+        # Calling the function with different numbers
+print(factorial_recursive(4))     # Output: 24 (4*3*2*1)
+print(factorial_recursive(1))     # Output: 1 (base case)
+print(factorial_recursive(6))     # Output: 720 (6*5*4*3*2*1)
+
