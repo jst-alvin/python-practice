1a6e883fa57c4cdf64c88b884964484284f5ef1b
Alvin Mugoya <mugoyaalvin018@gmail.com>
2025-04-25 13:52:06 +0300

    Create even_or_odd.py

---
+++ even_or_odd.py
@@ -0,0 +1,6 @@
+def check_even_odd(num):
+    # If number is divisible by 2 with no remainder, it's even
+    if num % 2 == 0:
+        return "Even"
+    else:  # Otherwise, it's odd
+        return "Odd"
